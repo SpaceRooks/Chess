d71d399d27441b7d0531ea4b353898c6897d605c
GregCart <35046892+GregCart@users.noreply.github.com>
2020-07-09 16:48:51 -0500

    checkers

--- chess.js
+++ chess.js
@@ -6,6 +6,7 @@ const side = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 function makeBoard() {
     let brd = document.createDocumentFragment();
+    let color = true;
     for (var i = 1; i < side.length + 1; i++) {
         let row = document.createElement("div");
         row.setAttribute("id", i.toString());
@@ -14,8 +15,20 @@ function makeBoard() {
             let box = document.createElement("div");
             box.setAttribute("id", "" + side[r] + i.toString() + "");
             box.setAttribute("class", "tile");
+            if (color == true) {
+                box.style.backgroundColor = "blue";
+                color = false;
+             } else {
+                box.style.backgroundColor = "red";
+                color = true;
+             }
             row.appendChild(box);
         }
+        if (color == true) {
+            color = false;
+         } else {
+            color = true;
+         }
         brd.appendChild(row);
     }
     board.appendChild(brd);
