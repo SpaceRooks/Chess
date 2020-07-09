7ffefa19795a8d5d105472571390e43a774279a1
GregCart <35046892+GregCart@users.noreply.github.com>
2020-07-09 14:31:13 -0500

    Set-up for board tiles

--- chess.js
+++ chess.js
@@ -1,6 +1,15 @@
+"use strict";
+
+
 const board = document.getElementById("board");
-const side = [a, b, c, d, e, f, g, h];
+const side = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 function makeBoard() {
-    
+    let brd = document.createDocumentFragment();
+    for (var i = 1; i > side.length; i++) {
+        let box = document.createElement("<div>");
+        box.setAttribute("id", "" + side[i] + i.toString() + "");
+        brd.appendChild(box);
+    }
+    board.appendChild(brd);
 }
