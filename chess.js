ee8d512daa21b26f582b9a758f97d39cf0cb4cc7
GregCart <35046892+GregCart@users.noreply.github.com>
2020-07-09 15:31:09 -0500

    updated board

--- chess.js
+++ chess.js
@@ -6,10 +6,20 @@ const side = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 function makeBoard() {
     let brd = document.createDocumentFragment();
-    for (var i = 1; i > side.length; i++) {
-        let box = document.createElement("<div>");
-        box.setAttribute("id", "" + side[i] + i.toString() + "");
-        brd.appendChild(box);
+    for (var i = 1; i < side.length + 1; i++) {
+        let row = document.createElement("div");
+        row.setAttribute("id", i.toString());
+        row.setAttribute("class", "rank");
+        for (var r = 0; r < side.length; r++) {
+            let box = document.createElement("div");
+            box.setAttribute("id", "" + side[r] + i.toString() + "");
+            box.setAttribute("class", "tile");
+            row.appendChild(box);
+        }
+        brd.appendChild(row);
     }
     board.appendChild(brd);
 }
+
+
+document.body.onload(makeBoard());
