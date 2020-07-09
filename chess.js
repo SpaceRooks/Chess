ba15599e4815d6e87e07f66f6c8966786bc5e34a
GregCart <35046892+GregCart@users.noreply.github.com>
2020-07-08 21:05:55 -0500

    use flexinstead

--- chess.js
+++ chess.js
@@ -1,15 +1,6 @@
-var board = document.getElementById("board");
-var side = [a, b, c, d, e, f, g, h];
+const board = document.getElementById("board");
+const side = [a, b, c, d, e, f, g, h];
 
 function makeBoard() {
-    var brd = document.createDocumentFragment();
-    for (var i = 0; i < 8; i++) {
-        var tr = document.createElement("<tr>");
-        tr.setAttribute("id", i + 1);
-        for (var c = 0; c < side.length; c++) {
-            var td = document.createElement("<td>");
-            td.setAttribute("id", "" + side[c].toString() + c+1 + "");
-            tr.appendChild(td);
-        }
-    }
+    
 }
