28a05dd3962b17855f0fc518c2bee42c1dd5e074
GregCart <35046892+GregCart@users.noreply.github.com>
2020-07-20 18:28:24 -0500

    comments

--- chess.js
+++ chess.js
@@ -7,13 +7,15 @@ const side = ["a", "b", "c", "d", "e", "f", "g", "h"];
 function makeBoard() {
     let brd = document.createDocumentFragment();
     let color = true;
+//   create rows
     for (var i = 1; i < side.length + 1; i++) {
         let row = document.createElement("div");
         row.setAttribute("id", i.toString());
         row.setAttribute("class", "rank");
+//   create the cells
         for (var r = 0; r < side.length; r++) {;
             let box = document.createElement("div");
-            box.setAttribute("id", "" + side[r] + i.toString() + "");
+            box.setAttribute("id", side[r] + i.toString());
             box.setAttribute("name", i.toString());
             box.setAttribute("class", "tile");
             if (color == true) {
@@ -32,6 +34,7 @@ function makeBoard() {
          }
         brd.appendChild(row);
     }
+//   add to the board
     board.appendChild(brd);
     setBoard();
 }
@@ -45,11 +48,14 @@ function setBoard() {
     var last = document.getElementsByName("8");
     var all = [one, last];
     var src;
+//   setup pieces based on color
     for (var i = 0; i < all.length; i++) {
         var srcP = "Chess_Peices/" + n[i] + "/";
+//   setup each piece
         for (var t = 0; t < all[i].length; t++) {
             var img = document.createElement("img");
             var piece = all[i][t];
+//   choose which piece is which
             switch (piece.id) {
                 case side[0] + num[i]:
                 case side[7] + num[i]:
@@ -70,6 +76,7 @@ function setBoard() {
                     src = srcP + n[i].toLowerCase() + "K.png";
                     break;
             }
+//   add it to the board
             img.setAttribute("src", src);
             img.setAttribute("id", n[i] + i.toString() + "");
             piece.appendChild(img);
@@ -82,9 +89,11 @@ function setPawns() {
     var bPs = document.getElementsByName("7")
     var t = 0;
     var src;
+// choose white or black
     while (t < 2) {
         for (var i = 0; i < wPs.length; i++) {  
             var img = document.createElement("img");
+//   choose sorce and id
             if (t == 0) {
                 src = "Chess_Peices/W/wP.png";
                 var tile = wPs[i];
