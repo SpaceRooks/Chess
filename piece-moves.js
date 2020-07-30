6afe083da2ae07c33766d950c175e75bb4430865
GregCart <35046892+GregCart@users.noreply.github.com>
2020-07-30 11:59:13 -0500

    queen moves

--- piece-moves.js
+++ piece-moves.js
@@ -213,116 +213,152 @@ function checkQueen(piece, cSpot) {
     let u = parseInt(tile[1]);
     let b = [(u-7).toString(), (u-6).toString(), (u-5).toString(), (u-4).toString(), (u-3).toString(), (u-2).toString(), (u-1).toString(), (u).toString(), (u+1).toString(), (u+2).toString(), (u+3).toString(), (u+4).toString(), (u+5).toString(), (u+6).toString(), (u+7).toString()];
     let c = [a, b];
+    let f = side[n];
+    let d = document.getElementById(tile[1]).childNodes;
     let move1 = true;
     let move2 = true;
-    b.forEach(element => {
-        part1: {
-            if (move1 == true) {
-                let m = element;
-                if (m.id == piece.parentElement.id) {
-                    break part1;
-                } else {
-                    if (m.hasChildNodes() == true) {
-                        if (getID(m.childNodes[0])[0] == getID(piece)[0]) {
-                            move1 = false;
-                            break part1;
-                        } else {
-                            takes.push(m);
-                            moves.push(m);
-                        }
-                    } else {
-                        moves.push(m);
-                    }
-                }
-            } else {
-                break part1;
-            }
-        }
-        part2: {
-            if (move2 == true) {
-                let m = document.getElementById(a + i.toString());
-                if (m == null || m == undefined) {
-                    break part2;
-                } else {
-                    if (m.id == piece.parentElement.id) {
-                        break part2;
-                    } else {
-                        if (m.hasChildNodes() == true) {
-                            if (getID(m.childNodes[0])[0] == getID(piece)[0]) {
-                                move2 = false;
-                                break part2;
-                            } else {
-                                takes.push(m);
-                                moves.push(m);
-                            }
-                        } else {
-                            moves.push(m);
-                        }
-                    }
-                }
-            } else {
-                break part2;
-            }
-        }
-        i++;
-    })
-    let a = side[n];
-    let u = parseInt(tile[1]);
-    let b = document.getElementById(tile[1]).childNodes;
-    let l = 0;
     let move3 = true;
     let move4 = true;
-    b.forEach(element => {
-        part3: {
-            if (move3 == true) {
-                let m = element;
-                if (m.id == piece.parentElement.id) {
-                    break part3;
-                } else {
-                    if (m.hasChildNodes() == true) {
-                        if (getID(m.childNodes[0])[0] == getID(piece)[0]) {
-                            move3 = false;
-                            break part3;
+    // diagonals
+    loop1: {
+        for (let i = 0; i < b.length; i++)  {
+            // diagnal
+            part1: {
+                if (move1 == true) {
+                    let m = document.getElementById(a[i] + b[i]);
+                    if (m == null || m == undefined) {
+                        break part1;
+                    } else {
+                        if (m.id == piece.parentElement.id) {
+                            break part1;
                         } else {
-                            takes.push(m);
-                            moves.push(m);
-                        }
-                    } else {
-                        moves.push(m);
-                    }
-                }
-            } else {
-                break part3;
-            }
-        }
-        part4: {
-            if (move4 == true) {
-                let m = document.getElementById(a + l.toString());
-                if (m == null || m == undefined) {
-                    break part4;
-                } else {
-                    if (m.id == piece.parentElement.id) {
-                        break part4;
-                    } else {
-                        if (m.hasChildNodes() == true) {
-                            if (getID(m.childNodes[0])[0] == getID(piece)[0]) {
-                                move4 = false;
-                                break part4;
+                            if (m.hasChildNodes() == true) {
+                                if (getID(m.childNodes[0])[0] == getID(piece)[0] && parseInt(getID(m)[1]) < parseInt(tile[1])) {
+                                    break part1;
+                                } else if (getID(m.childNodes[0])[0] == getID(piece)[0] && parseInt(getID(m)[1]) > parseInt(tile[1])) {
+                                    move1 = false;
+                                    break part1;
+                                } else {
+                                    takes.push(m);
+                                    moves.push(m);
+                                }
                             } else {
-                                takes.push(m);
                                 moves.push(m);
                             }
-                        } else {
-                            moves.push(m);
                         }
                     }
+                } else {
+                    break part1;
                 }
+            }
+            part2: {
+                //other diagonal
+                if (move2 == true) {
+                    let p = document.getElementById(a[a.length - 1 - i] + b[i]);
+                    if (p == null || p == undefined) {
+                        break part2;
+                    } else {
+                        if (p == null || p == undefined) {
+                            break part2;
+                        } else {
+                            if (p.id == piece.parentElement.id) {
+                                break part2;
+                            } else {
+                                if (p.hasChildNodes() == true) {
+                                    if (getID(p.childNodes[0])[0] == getID(piece)[0] && parseInt(getID(p)[1]) < parseInt(tile[1])) {
+                                        break part2;
+                                    } else if (getID(p.childNodes[0])[0] == getID(piece)[0] && parseInt(getID(p)[1]) > parseInt(tile[1])) {
+                                        move2 = false;
+                                        break part2;
+                                    } else {
+                                        takes.push(p);
+                                        moves.push(p);
+                                    }
+                                } else {
+                                    moves.push(p);
+                                }
+                            }
+                        }
+                    }
+                } else {
+                    break part2;
+                }
+            }
+            if (move1 == false && move2 == false) {
+                break loop1;
             } else {
-                break part4;
+                
             }
         }
-        l++;
-    })
+    }
+    //side to side and up and down
+    loop2: {
+        for (let l = 0; l < d.length; l++) {
+            //side to side
+            part3: {
+                if (move3 == true) {
+                    let m = d[l];
+                    if (m == null || m == undefined) {
+                        break part3;
+                    } else {
+                        if (m.id == piece.parentElement.id) {
+                            break part3;
+                        } else {
+                            if (m.hasChildNodes() == true) {
+                                if (getID(m.childNodes[0])[0] == getID(piece)[0] && side.indexOf(getID(m)[1]) < side.indexOf(tile[1])) {
+                                    break part3;
+                                } else if (getID(m.childNodes[0])[0] == getID(piece)[0] && side.indexOf(getID(m)[1]) > side.indexOf(tile[1])) {
+                                    move3 = false;
+                                    break part3;
+                                } else {
+                                    takes.push(m);
+                                    moves.push(m);
+                                }
+                            } else {
+                                moves.push(m);
+                            }
+                        }
+                    }
+                } else {
+                    break part3;
+                }
+            }
+            part4: {
+                // up and down
+                if (move4 == true) {
+                    let m = document.getElementById(f + l.toString());
+                    if (m == null || m == undefined) {
+                        break part4;
+                    } else {
+                        if (m.id == piece.parentElement.id) {
+                            break part4;
+                        } else {
+                            if (m.hasChildNodes() == true) {
+                                if (getID(m.childNodes[0])[0] == getID(piece)[0] && parseInt(getID(m)[1]) < parseInt(tile[1])) {
+                                    break part4;
+                                } else if (getID(m.childNodes[0])[0] == getID(piece)[0] && parseInt(getID(m)[1]) > parseInt(tile[1])) {
+                                    move4 = false;
+                                    break part4;
+                                } else {
+                                    takes.push(m);
+                                    moves.push(m);
+                                }
+                            } else {
+                                moves.push(m);
+                            }
+                        }
+                    }
+                } else {
+                    break part4;
+                }
+            }
+            if (move3 == false && move4 == false) {
+                break loop2;
+            } else {
+                
+            }
+        }
+    }
 }
 
 function checkKing(piece, cSpot) {
